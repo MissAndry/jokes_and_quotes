5646b06e48661b97f99d65080bd3b68b3a7fe295
Megan Gaebler <megan.gaebler@gmail.com>
2015-02-13 00:11:21 -0600

    took db calls out of the view

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,23 +1,6 @@
 module PostHelper
-  def nested_comments(comments)
-    top_level = comments
-    sub_comments = []
-    collect = []
-    
-    comments.each do |comment|
-      shared_commentables = Comment.where(commentable_id: comment.commentable_id, commentable_type: comment.commentable_type)
-      collect << comment
-      if comment.has_comments?
-        sub_comments += comment.comments
-      else
-        sub_comments += shared_commentables
-      end
-    end
-
-    if sub_comments.last == top_level.last || ((sub_comments.last.commentable_id == top_level.last.id) && (sub_comments.last.commentable_type == "Post"))
-      return collect.uniq
-    end
-    nested_comments(sub_comments)
+  def no_posts?
+    Post.all.empty?
   end
 end
 
