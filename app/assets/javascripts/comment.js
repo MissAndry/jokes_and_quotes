25afcf59dbe059a1c3c59730fe986a7bbb3c65f0
Megan Gaebler <megan.gaebler@gmail.com>
2015-02-17 15:19:27 -0600

    refactored adding comments js slightly. no less complex but at least it's not all in document.ready()

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,74 +1,80 @@
-function showCommentInputForm(response, buttonID, button){
-  $('#comment-' + buttonID + '-input').append(response);
-  button.hide();
+function showMainCommentInputForm(button){
+  var url, request;
+  
+  url = button.find('a').attr('href');
+  request = $.get( url );
+
+  request.done(function(response){
+    $('#main-add-comment-input').append(response);
+    button.hide();
+    if ( $('.edit_post_button') ) {
+      $('.edit_post_button').hide();
+    }
+  });
+}
+
+function addComment(form){
+  var url, comment;
+  url = form.attr('action');
+  comment = form.serialize();
+  request = $.post( url, comment );
+
+  request.done(function(response){
+    $('.add-comment.button').show();
+    form.hide();
+    $('#comments-container').append(response);
+  });
+}
+
+function showCommentInputForm(button){
+  var buttonID, url, request
+  
+  buttonID = button.attr('id');
+  url = button.find('a').attr('href');
+  request = $.get( url );
+
+  request.done(function(response){
+    $('#comment-' + buttonID + '-input').append(response);
+    button.hide();
+  });
+}
+
+function addSubComment(form){
+  var url, comment;
+  url = form.attr('action');
+  comment = form.serialize();
+
+  request = $.post( url, comment );
+
+  request.done(function(response){
+    form.hide();
+    $('.sub-comment-link').show();
+    form.parent().parent().siblings('ul').append(response);
+  });
+  
 }
 
 $(document).ready(function(){
   
   $('.add-comment').on('click', function(e){
-    var button, url, request;
-
     e.preventDefault();
-    button = $(this);
-    
-    url = button.find('a').attr('href');
-    request = $.get( url );
-
-    request.done(function(response){
-      $('#main-add-comment-input').append(response);
-      button.hide();
-      if ( $('.edit_post_button') ) {
-       $('.edit_post_button').hide();
-      }
-    });
+    showMainCommentInputForm($(this));
   });
 
   $('#main-add-comment-input').on('submit', '.add-comment', function(e){
-    var url, comment, form;
     e.preventDefault();
-    form = $(this);
-    url = form.attr('action');
-    comment = form.serialize();
-    request = $.post( url, comment );
-
-    request.done(function(response){
-      $('.add-comment.button').show();
-      form.hide();
-      $('#comments-container').append(response);
-    });
+    addComment($(this));
     return false;
   });
 
   $('#comments-container').on('click', '.sub-comment-link', function(e){
-    var button, url, request;
     e.preventDefault();
-
-    button = $(this);
-
-    var buttonID = button.attr('id');
-    url = button.find('a').attr('href');
-    request = $.get( url );
-
-    request.done(function(response){
-      showCommentInputForm(response, buttonID, button);
-    });
+    showCommentInputForm($(this));
   });
 
   $('#comments-container').on('submit', '.add-comment', function(e){
-    var url, comment, form;
     e.preventDefault();
-    form = $(this);
-    url = form.attr('action');
-    comment = form.serialize();
-
-    request = $.post( url, comment );
-
-    request.done(function(response){
-      console.log(form.siblings());
-      form.hide();
-      $('.sub-comment-link').show();
-      form.parent().parent().siblings('ul').append(response);
-    });
+    addSubComment($(this));
     return false;
   });
 
